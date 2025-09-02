8176bc26983f9d65bd14713d60a3bef1b95e546e
TheChupa <160878340+TheChupa@users.noreply.github.com>
2025-09-02 18:40:31 -0500

    initial commit

---
+++ CyberCrimeDashBoard/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        Placeholder Text
+
+        This is a placeholder for the Cyber Crime Dashboard application.
+
+        I am going to slowly build this out with information from Operation Shamrock and St. Louis Data
+
+
+        Will do my best to make it accessible for each neighborhood and City.
+
+
+        TODO:
+<br></br>
+        Find data sources from Shamrock
+        <br></br>
+        find data sources from St. Louis Data 
+        <br></br>
+        Create visualizations for each data source, maybe compare St louis to total money vs us, do the same for scams lost.
+        <br></br>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
